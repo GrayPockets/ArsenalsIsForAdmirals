cd756f6da938bfa03fc7c1ead572f12bb2374281
Matthew J. H. Peronto <matthew.peronto@gmail.com>
2024-03-28 22:29:28 -0700

    Arsenal Project Icon
    * Arsenal project now has its own icon

--- SQL/Icon/ArsenalsIsForAdmirals.sql
+++ SQL/Icon/ArsenalsIsForAdmirals.sql
@@ -3,7 +3,19 @@
 DELETE FROM "IconDefinitions"
 WHERE "Name" = 'ICON_PROJECT_ENHANCE_DISTRICT_HARBOR';
 
-INSERT OR REPLACE INTO "IconDefinitions"
-	(	"Name",										"Atlas",				"Index"	)
+INSERT INTO "IconTextureAtlases"
+	(	"Name",							"IconSize",	"IconsPerRow",	"IconsPerColumn",	"FileName"				)
 VALUES
-	(	'ICON_PROJECT_ENHANCE_DISTRICT_ARSENAL',	'ICON_ATLAS_PROJECTS',	12		);
+	(	'ICON_ATLAS_ARSENAL_PROJECTS',	256,		1,				1,					'ArsenalProject_256'	),
+	(	'ICON_ATLAS_ARSENAL_PROJECTS',	128,		1,				1,					'ArsenalProject_128'	),
+	(	'ICON_ATLAS_ARSENAL_PROJECTS',	80,			1,				1,					'ArsenalProject_80'		),
+	(	'ICON_ATLAS_ARSENAL_PROJECTS',	70,			1,				1,					'ArsenalProject_70'		),
+	(	'ICON_ATLAS_ARSENAL_PROJECTS',	50,			1,				1,					'ArsenalProject_50'		),
+	(	'ICON_ATLAS_ARSENAL_PROJECTS',	38,			1,				1,					'ArsenalProject_38'		),
+	(	'ICON_ATLAS_ARSENAL_PROJECTS',	32,			1,				1,					'ArsenalProject_32'		),
+	(	'ICON_ATLAS_ARSENAL_PROJECTS',	30,			1,				1,					'ArsenalProject_30'		);
+
+INSERT OR REPLACE INTO "IconDefinitions"
+	(	"Name",										"Atlas",						"Index"	)
+VALUES
+	(	'ICON_PROJECT_ENHANCE_DISTRICT_ARSENAL',	'ICON_ATLAS_ARSENAL_PROJECTS',	0		);
